d6441be3aad7e035911e39303358c5f3fe815e94
Joel Ringh <joel.ringh@gmail.com>
2026-09-13 18:16:58 +0200

    Error fix and push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import List from "./components/List";
+import List from "./components/list";
 import Footer from "./components/Footer";
 
 function App() {

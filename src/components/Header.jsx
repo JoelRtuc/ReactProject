66aee44b0f7793dc6727110d90a2747bb0715de3
Joel Ringh <joel.ringh@gmail.com>
2026-09-22 18:14:49 +0200

    Yes, profile pictures now work.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import './HeaderStyle.css'
 import minordleSymbol from "./MinordleSymbol.png";
+import { baseUrl } from './api';
 
 function Header({user}) {
   return (
     <>
     <header className="header">
+      {user ? (<> <img src={`${baseUrl}${user.profilePicturePath}`} width="50" alt="profile" />
+      <div>{user.username}</div>  </>) : <></>}
       <img src={minordleSymbol} alt="MinordleSymbol" className='minordleSymbol' />
     </header>
     </>
